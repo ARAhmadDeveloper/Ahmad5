4402b3f2392e4e035ded0607017196b64bcbafea
ArAhmadRaza <165558272+ArAhmadRaza@users.noreply.github.com>
2024-04-30 16:52:14 +0500

    JavaScript Classes
    JavaScript Classes

---
+++ JavaScript/Javascript classes/Class no 3/Script.js
@@ -0,0 +1,63 @@
+// var num = 10;
+
+// var num1 = 20;
+
+// num = 30;           //This is use to change the value of Data which is given in the "var num = 10"
+// // Now we cannot the previous value of num because of replacement
+// console.log(num + num1)
+
+// var post = 50;
+// post++              //this is the "Post increment Operator" post++=post + 1
+// // This will Add one in the real Data and replace it
+// post++
+// console.log("Post", post)
+
+// var post1 = 40;
+// post1 = post1 + 1;              //For Add One
+// post1++                         //For Add One
+// console.log("post1", post1)
+
+// var num2 = 25;
+// num2--                  //This is "Post Decrement Operator"
+// // This will Decrease 1 from Data and replace it with Data
+// console.log("num2", num2)
+
+// var num3 = 60;
+// var output = num3 + 2;
+// num3 = num3 + 1;
+// num3--
+// console.log("output", output);
+// console.log("num3", num3)
+
+
+// var num4 = 70;
+// var result = num4++
+// console.log("result", result)       // It will show the value 70 not 71 
+// console.log("num4", num4)
+
+
+// var num5 = 80;
+// var result1 = num5++      // It will show the value 80 not 81
+// result1--
+// console.log("result1", result1)
+
+
+
+var num6 = 90;
+++num6           // It is "pre increment operator"
+console.log("num6", num6)
+
+
+var num7 = 100;
+var result2 = ++num7;
+console.log("result2", result2)
+
+
+var num8 = 110;
+--num8              // It is "pre Discrement operator"
+console.log("num8", num8)
+
+
+var num9 = 120;
+var result3 = --num9;
+console.log("result3", result3)
