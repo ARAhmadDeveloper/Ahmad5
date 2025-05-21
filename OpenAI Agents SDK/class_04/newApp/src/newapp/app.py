46926b23d466c2ebcac2a321afb2d8272bc1aa45
ArAhmadRaza <arahmadraza5570@gmail.com>
2025-05-21 17:05:49 +0500

    Hello

--- OpenAI Agents SDK/class_04/newApp/src/newapp/app.py
+++ OpenAI Agents SDK/class_04/newApp/src/newapp/app.py
@@ -4,7 +4,6 @@ from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
 from agents.run import RunConfig
 import asyncio
 
-
 # Load the environment variables from the .env file
 load_dotenv()
 
@@ -13,11 +12,6 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 # Check if the API key is present; if not, raise an error
 if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
-
-
-
-
-
 
 
 external_client = AsyncOpenAI(
@@ -35,9 +29,6 @@ config = RunConfig(
     model_provider=external_client,
     tracing_disabled=True
 )
-
-
-
 
 async def main():
     agent = Agent(
