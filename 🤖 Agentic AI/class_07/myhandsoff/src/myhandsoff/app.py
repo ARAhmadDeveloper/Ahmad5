efba219d0952485417d5e13fa343c0ef9ccaf730
ArAhmadRaza <arahmadraza5570@gmail.com>
2025-05-24 13:55:29 +0500

    Agentic AI

--- 🤖 Agentic AI/class_07/myhandsoff/src/myhandsoff/app.py
+++ 🤖 Agentic AI/class_07/myhandsoff/src/myhandsoff/app.py
@@ -1,8 +1,9 @@
 import os
-import google.generativeai as genai
 from dotenv import load_dotenv
+import google.generativeai as genai
 
 load_dotenv()
+
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 # ====== Tools ======
