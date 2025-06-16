5b49cad5128b0b2752de6659261e0b75398ab2b9
FISTA Solutions <fistasolutions@gmail.com>
2025-06-16 18:54:50 +0500

    modified:

--- 🌑NextJS/nextjs-dashboard/app/lib/actions.ts
+++ 🌑NextJS/nextjs-dashboard/app/lib/actions.ts
@@ -4,21 +4,45 @@ import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: "Please enter an amount greater than 0",
+  }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status",
+  }),
   date: z.string(),
 });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+};
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
 
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
@@ -35,46 +59,43 @@ export async function createInvoice(formData: FormData) {
   `;
   } catch (error) {
     console.log(error);
-
   }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
-  
 }
-
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
   });
- 
+
   const amountInCents = amount * 100;
- 
- try{
+
+  try {
     await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- } catch (error) {
+  } catch (error) {
     console.log(error);
- }
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-    try {
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    console.log(error);
   }
+}
--- 🐍Python/fastApi with MySql/database.py
+++ 🐍Python/fastApi with MySql/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
 
-DATABASE_URL = "mysql+pymysql://root:Ahmad%405570@localhost:3306/fastapi_with_mysql"
+password = quote_plus("Ahmad@5570")
+DATABASE_URL = f"mysql+pymysql://root:{password}@localhost:3306/fastapi_with_mysql"
 
 engine = create_engine(DATABASE_URL)
 
