caf0c7fb604ab0eab3d481695a0a0145a6e5bd74
Ahmad Raza <ahmed.ainex@gmail.com>
2026-02-19 17:30:05 +0500

    modified

---
+++ 🌑NextJS/my-app/app/animation/page.tsx
@@ -0,0 +1,17 @@
+// BlobBackground.jsx
+import React from "react";
+import "./backgroundBlob.css"; // external CSS for clean editing
+
+const BubbleBackground = () => {
+  return (
+    <div className="huzi-hero-bg">
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+    </div>
+  );
+};
+
+export default BubbleBackground;
